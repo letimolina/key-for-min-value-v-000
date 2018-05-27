7f574661499c6d00f6907303da521a72404f93a4
Leticia Molina <letimolina@gmail.com>
2018-05-27 19:47:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
 min_val_key = nil
 min_value = 9999
 name_hash.each do |key, value|
-  if name_hash[key] < min_value
-    min_value = name_hash[key]
+  if value < min_value
+    min_value = value
     min_val_key = key
   end
 end
